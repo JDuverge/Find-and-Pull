e28a87c29ea3016228e6cce35705db62948cadbf
JDuverge <jduverge1020@gmail.com>
2010-05-28 18:19:52 -0400

    fixed file dir

---
+++ FindAndPull/MainForm.Designer.cs
@@ -0,0 +1,317 @@
+﻿namespace FindAndPull
+{
+    partial class mainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.menuStrip = new System.Windows.Forms.MenuStrip();
+            this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.preferencesToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripSeparator2 = new System.Windows.Forms.ToolStripSeparator();
+            this.exitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.helpToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.changelogToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripSeparator1 = new System.Windows.Forms.ToolStripSeparator();
+            this.aboutToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.statusStrip1 = new System.Windows.Forms.StatusStrip();
+            this.preferencesToolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
+            this.changelogToolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
+            this.aboutToolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
+            this.textBoxSearch = new System.Windows.Forms.TextBox();
+            this.labelSearch = new System.Windows.Forms.Label();
+            this.buttonSearch = new System.Windows.Forms.Button();
+            this.checkedListBoxLocations = new System.Windows.Forms.CheckedListBox();
+            this.buttonPull = new System.Windows.Forms.Button();
+            this.labelSearch2 = new System.Windows.Forms.Label();
+            this.textBoxSearch2 = new System.Windows.Forms.TextBox();
+            this.buttonSearch2 = new System.Windows.Forms.Button();
+            this.buttonReset = new System.Windows.Forms.Button();
+            this.toolStripStatusLabel1 = new System.Windows.Forms.ToolStripStatusLabel();
+            this.menuStrip.SuspendLayout();
+            this.statusStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip
+            // 
+            this.menuStrip.Font = new System.Drawing.Font("Century Gothic", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.menuStrip.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileToolStripMenuItem,
+            this.helpToolStripMenuItem});
+            this.menuStrip.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip.Name = "menuStrip";
+            this.menuStrip.Padding = new System.Windows.Forms.Padding(16, 5, 0, 5);
+            this.menuStrip.Size = new System.Drawing.Size(984, 38);
+            this.menuStrip.TabIndex = 0;
+            // 
+            // fileToolStripMenuItem
+            // 
+            this.fileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.preferencesToolStripMenuItem,
+            this.toolStripSeparator2,
+            this.exitToolStripMenuItem});
+            this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            this.fileToolStripMenuItem.ShortcutKeys = ((System.Windows.Forms.Keys)((System.Windows.Forms.Keys.Alt | System.Windows.Forms.Keys.F)));
+            this.fileToolStripMenuItem.Size = new System.Drawing.Size(54, 28);
+            this.fileToolStripMenuItem.Text = "File";
+            // 
+            // preferencesToolStripMenuItem
+            // 
+            this.preferencesToolStripMenuItem.Name = "preferencesToolStripMenuItem";
+            this.preferencesToolStripMenuItem.Size = new System.Drawing.Size(202, 28);
+            this.preferencesToolStripMenuItem.Text = "Preferences";
+            this.preferencesToolStripMenuItem.Click += new System.EventHandler(this.preferencesToolStripMenuItem_Click);
+            // 
+            // toolStripSeparator2
+            // 
+            this.toolStripSeparator2.Name = "toolStripSeparator2";
+            this.toolStripSeparator2.Size = new System.Drawing.Size(199, 6);
+            // 
+            // exitToolStripMenuItem
+            // 
+            this.exitToolStripMenuItem.Name = "exitToolStripMenuItem";
+            this.exitToolStripMenuItem.Size = new System.Drawing.Size(202, 28);
+            this.exitToolStripMenuItem.Text = "Exit";
+            this.exitToolStripMenuItem.Click += new System.EventHandler(this.exitToolStripMenuItem_Click);
+            // 
+            // helpToolStripMenuItem
+            // 
+            this.helpToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.changelogToolStripMenuItem,
+            this.toolStripSeparator1,
+            this.aboutToolStripMenuItem});
+            this.helpToolStripMenuItem.Name = "helpToolStripMenuItem";
+            this.helpToolStripMenuItem.ShortcutKeys = ((System.Windows.Forms.Keys)((System.Windows.Forms.Keys.Alt | System.Windows.Forms.Keys.H)));
+            this.helpToolStripMenuItem.Size = new System.Drawing.Size(69, 28);
+            this.helpToolStripMenuItem.Text = "Help";
+            // 
+            // changelogToolStripMenuItem
+            // 
+            this.changelogToolStripMenuItem.Name = "changelogToolStripMenuItem";
+            this.changelogToolStripMenuItem.Size = new System.Drawing.Size(198, 28);
+            this.changelogToolStripMenuItem.Text = "Changelog";
+            this.changelogToolStripMenuItem.Click += new System.EventHandler(this.changelogToolStripMenuItem_Click);
+            // 
+            // toolStripSeparator1
+            // 
+            this.toolStripSeparator1.Name = "toolStripSeparator1";
+            this.toolStripSeparator1.Size = new System.Drawing.Size(195, 6);
+            // 
+            // aboutToolStripMenuItem
+            // 
+            this.aboutToolStripMenuItem.Name = "aboutToolStripMenuItem";
+            this.aboutToolStripMenuItem.Size = new System.Drawing.Size(198, 28);
+            this.aboutToolStripMenuItem.Text = "About...";
+            this.aboutToolStripMenuItem.Click += new System.EventHandler(this.aboutToolStripMenuItem_Click);
+            // 
+            // statusStrip1
+            // 
+            this.statusStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripStatusLabel1});
+            this.statusStrip1.Location = new System.Drawing.Point(0, 515);
+            this.statusStrip1.Name = "statusStrip1";
+            this.statusStrip1.Size = new System.Drawing.Size(984, 22);
+            this.statusStrip1.TabIndex = 1;
+            this.statusStrip1.Text = "statusStrip1";
+            // 
+            // preferencesToolStripMenuItem1
+            // 
+            this.preferencesToolStripMenuItem1.Name = "preferencesToolStripMenuItem1";
+            this.preferencesToolStripMenuItem1.Size = new System.Drawing.Size(202, 28);
+            this.preferencesToolStripMenuItem1.Text = "Preferences";
+            // 
+            // changelogToolStripMenuItem1
+            // 
+            this.changelogToolStripMenuItem1.Name = "changelogToolStripMenuItem1";
+            this.changelogToolStripMenuItem1.Size = new System.Drawing.Size(202, 28);
+            this.changelogToolStripMenuItem1.Text = "Changelog";
+            // 
+            // aboutToolStripMenuItem1
+            // 
+            this.aboutToolStripMenuItem1.Name = "aboutToolStripMenuItem1";
+            this.aboutToolStripMenuItem1.Size = new System.Drawing.Size(201, 28);
+            this.aboutToolStripMenuItem1.Text = "About";
+            // 
+            // textBoxSearch
+            // 
+            this.textBoxSearch.Location = new System.Drawing.Point(182, 114);
+            this.textBoxSearch.Name = "textBoxSearch";
+            this.textBoxSearch.Size = new System.Drawing.Size(487, 41);
+            this.textBoxSearch.TabIndex = 2;
+            // 
+            // labelSearch
+            // 
+            this.labelSearch.AutoSize = true;
+            this.labelSearch.Location = new System.Drawing.Point(176, 78);
+            this.labelSearch.Name = "labelSearch";
+            this.labelSearch.Size = new System.Drawing.Size(493, 33);
+            this.labelSearch.TabIndex = 3;
+            this.labelSearch.Text = "Type in what file you want to search:";
+            // 
+            // buttonSearch
+            // 
+            this.buttonSearch.Location = new System.Drawing.Point(735, 113);
+            this.buttonSearch.Name = "buttonSearch";
+            this.buttonSearch.Size = new System.Drawing.Size(100, 41);
+            this.buttonSearch.TabIndex = 4;
+            this.buttonSearch.Text = "Find!";
+            this.buttonSearch.UseVisualStyleBackColor = true;
+            this.buttonSearch.Click += new System.EventHandler(this.buttonSearch_Click);
+            // 
+            // checkedListBoxLocations
+            // 
+            this.checkedListBoxLocations.CheckOnClick = true;
+            this.checkedListBoxLocations.FormattingEnabled = true;
+            this.checkedListBoxLocations.Location = new System.Drawing.Point(47, 264);
+            this.checkedListBoxLocations.Margin = new System.Windows.Forms.Padding(6, 3, 6, 3);
+            this.checkedListBoxLocations.Name = "checkedListBoxLocations";
+            this.checkedListBoxLocations.Size = new System.Drawing.Size(890, 184);
+            this.checkedListBoxLocations.TabIndex = 5;
+            this.checkedListBoxLocations.ThreeDCheckBoxes = true;
+            this.checkedListBoxLocations.Visible = false;
+            // 
+            // buttonPull
+            // 
+            this.buttonPull.Location = new System.Drawing.Point(367, 454);
+            this.buttonPull.Name = "buttonPull";
+            this.buttonPull.Size = new System.Drawing.Size(100, 41);
+            this.buttonPull.TabIndex = 6;
+            this.buttonPull.Text = "Pull";
+            this.buttonPull.UseVisualStyleBackColor = true;
+            this.buttonPull.Visible = false;
+            this.buttonPull.Click += new System.EventHandler(this.buttonPull_Click);
+            // 
+            // labelSearch2
+            // 
+            this.labelSearch2.AutoSize = true;
+            this.labelSearch2.Location = new System.Drawing.Point(176, 163);
+            this.labelSearch2.Name = "labelSearch2";
+            this.labelSearch2.Size = new System.Drawing.Size(291, 33);
+            this.labelSearch2.TabIndex = 7;
+            this.labelSearch2.Text = "Search Within Results:";
+            this.labelSearch2.Visible = false;
+            // 
+            // textBoxSearch2
+            // 
+            this.textBoxSearch2.Location = new System.Drawing.Point(182, 199);
+            this.textBoxSearch2.Name = "textBoxSearch2";
+            this.textBoxSearch2.Size = new System.Drawing.Size(487, 41);
+            this.textBoxSearch2.TabIndex = 8;
+            this.textBoxSearch2.Visible = false;
+            // 
+            // buttonSearch2
+            // 
+            this.buttonSearch2.Location = new System.Drawing.Point(695, 199);
+            this.buttonSearch2.Name = "buttonSearch2";
+            this.buttonSearch2.Size = new System.Drawing.Size(180, 41);
+            this.buttonSearch2.TabIndex = 9;
+            this.buttonSearch2.Text = "Find Again!";
+            this.buttonSearch2.UseVisualStyleBackColor = true;
+            this.buttonSearch2.Visible = false;
+            this.buttonSearch2.Click += new System.EventHandler(this.buttonSearch2_Click);
+            // 
+            // buttonReset
+            // 
+            this.buttonReset.Location = new System.Drawing.Point(521, 455);
+            this.buttonReset.Name = "buttonReset";
+            this.buttonReset.Size = new System.Drawing.Size(100, 41);
+            this.buttonReset.TabIndex = 10;
+            this.buttonReset.Text = "Reset";
+            this.buttonReset.UseVisualStyleBackColor = true;
+            this.buttonReset.Click += new System.EventHandler(this.buttonReset_Click);
+            // 
+            // toolStripStatusLabel1
+            // 
+            this.toolStripStatusLabel1.Font = new System.Drawing.Font("Century Gothic", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.toolStripStatusLabel1.Name = "toolStripStatusLabel1";
+            this.toolStripStatusLabel1.Size = new System.Drawing.Size(0, 17);
+            // 
+            // mainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(16F, 33F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(984, 537);
+            this.Controls.Add(this.buttonReset);
+            this.Controls.Add(this.buttonSearch2);
+            this.Controls.Add(this.textBoxSearch2);
+            this.Controls.Add(this.labelSearch2);
+            this.Controls.Add(this.buttonPull);
+            this.Controls.Add(this.checkedListBoxLocations);
+            this.Controls.Add(this.buttonSearch);
+            this.Controls.Add(this.labelSearch);
+            this.Controls.Add(this.textBoxSearch);
+            this.Controls.Add(this.statusStrip1);
+            this.Controls.Add(this.menuStrip);
+            this.Font = new System.Drawing.Font("Century Gothic", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.MainMenuStrip = this.menuStrip;
+            this.Margin = new System.Windows.Forms.Padding(8, 7, 8, 7);
+            this.MaximizeBox = false;
+            this.MaximumSize = new System.Drawing.Size(1000, 575);
+            this.MinimizeBox = false;
+            this.MinimumSize = new System.Drawing.Size(1000, 575);
+            this.Name = "mainForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Find and Pull™";
+            this.Activated += new System.EventHandler(this.mainForm_Activated);
+            this.Deactivate += new System.EventHandler(this.mainForm_Deactivate);
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.mainForm_FormClosing);
+            this.menuStrip.ResumeLayout(false);
+            this.menuStrip.PerformLayout();
+            this.statusStrip1.ResumeLayout(false);
+            this.statusStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip;
+        private System.Windows.Forms.StatusStrip statusStrip1;
+        private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem preferencesToolStripMenuItem1;
+        private System.Windows.Forms.ToolStripMenuItem changelogToolStripMenuItem1;
+        private System.Windows.Forms.ToolStripMenuItem helpToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem aboutToolStripMenuItem1;
+        private System.Windows.Forms.ToolStripMenuItem preferencesToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem changelogToolStripMenuItem;
+        private System.Windows.Forms.ToolStripSeparator toolStripSeparator1;
+        private System.Windows.Forms.ToolStripMenuItem aboutToolStripMenuItem;
+        private System.Windows.Forms.ToolStripSeparator toolStripSeparator2;
+        private System.Windows.Forms.ToolStripMenuItem exitToolStripMenuItem;
+        private System.Windows.Forms.TextBox textBoxSearch;
+        private System.Windows.Forms.Label labelSearch;
+        private System.Windows.Forms.Button buttonSearch;
+        private System.Windows.Forms.CheckedListBox checkedListBoxLocations;
+        private System.Windows.Forms.Button buttonPull;
+        private System.Windows.Forms.Label labelSearch2;
+        private System.Windows.Forms.TextBox textBoxSearch2;
+        private System.Windows.Forms.Button buttonSearch2;
+        private System.Windows.Forms.Button buttonReset;
+        private System.Windows.Forms.ToolStripStatusLabel toolStripStatusLabel1;
+    }
+}
+
